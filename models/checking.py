cd66df526877643df77b0b539415037fcc73e1c2
Nikhil Nagar <nagarnikhil050@gmail.com>
2025-08-20 20:14:24 +0530

    upload a ai trainded file

---
+++ models/checking.py
@@ -0,0 +1,20 @@
+# import h5py, os
+# import tensorflow as tf
+# print(tf.__version__)
+
+# path = "models/model.h5"
+# print("Exists:", os.path.exists(path))
+
+# try:
+#     with h5py.File(path, "r") as f:
+#         print("✅ Valid HDF5 model file")
+# except Exception as e:
+#     print("❌ Not a valid HDF5 file:", e)
+
+
+# import tensorflow as tf
+# import keras
+# print("TensorFlow version:", tf.__version__)
+# print("Keras version:", keras.__version__)
+
+
